08836ad06b51101454f9141b200d7bd6f190410f
kaperusov <kaperusov@gmail.com>
2015-02-18 18:39:41 +0300

    script for enabled site

--- Vagrantfile
+++ Vagrantfile
@@ -8,7 +8,7 @@ Vagrant.configure(VAGRANTFILE_API_VERSION) do |config|
   config.vm.define "grig" do |grig_box|
     grig_box.vm.box_url = "http://ergonlogic.com/files/boxes/debian-LAMP-current.box"
     grig_box.vm.box = "grig"
-    grig_box.vm.network :private_network, ip: '192.168.22.2'
+    grig_box.vm.network :private_network, ip: '111.111.111.111'
     grig_box.vm.hostname = "grig.ru"
     grig_box.vm.provider "virtualbox" do |v|
        v.customize ["modifyvm", :id,
--- install.sh
+++ install.sh
@@ -1,14 +1,53 @@
 #!/bin/sh
 
-SITE=yurist.ru
-WWW=/var/www
+#
+# -- init variables
+#
+IP_ADDRESS=111.111.111.111
+DOCUMENT_ROOT=/var/www
+SITE_NAME=yurist.ru
+SITE_HOME=${DOCUMENT_ROOT}/${SITE_NAME}/public_html
 
-sudo mkdir -p ${WWW}/${SITE}/public_html
-sudo chown -R $USER:$USER ${WWW}/${SITE}/public_html
-sudo chmod -R 755 ${WWW}
-sudo cp /etc/apache2/sites-available/default /etc/apache2/sites-available/${SITE}
-#sudo vim /etc/apache2/sites-available/yurist.ru
+#
+# -- create a home directory for future site
+#
+sudo mkdir -p ${SITE_HOME}
+sudo chown -R $USER:$USER ${SITE_HOME}
+sudo chmod -R 755 ${DOCUMENT_ROOT}
+cd
+ln -s ${SITE_HOME} ${SITE_NAME}
 
-sudo a2ensite ${SITE}
+#
+# -- enable site
+#
+sudo cp /etc/apache2/sites-available/default /etc/apache2/sites-available/${SITE_NAME}
+sudo sed -i "s#DocumentRoot ${DOCUMENT_ROOT}#DocumentRoot ${SITE_HOME}#" /etc/apache2/sites-available/${SITE_NAME}
+sudo a2ensite ${SITE_NAME}
 sudo service apache2 restart
-#sudo vim /etc/hosts
+
+#
+# -- add virtual address to hosts
+#
+sudo echo ${IP_ADDRESS}" \t "${SITE_NAME} >> /etc/hosts
+
+#
+# -- create test index.php page
+#
+TEST_PAGE=${SITE_HOME}/index.php
+cat >${TEST_PAGE} <<EOL
+<html>
+  <head>
+    <title>Welcome to ${SITE_NAME}!</title>
+  </head>
+  <body>
+    <h1>Success! The ${SITE_NAME} virtual host is working!</h1>
+    <?phpinfo();?>
+  </body>
+</html>
+EOL
+
+#
+# -- test
+#
+wget 127.0.0.1/${SITE_NAME}/public_html/index.php
+
